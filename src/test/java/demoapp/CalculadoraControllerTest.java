8c7678daa6a098607d11592f34c3fb6dc46da734
Domingo Gallardo <domingo.gallardo@ua.es>
2023-09-21 15:25:50 +0200

    Test controller

--- src/test/java/demoapp/CalculadoraControllerTest.java
+++ src/test/java/demoapp/CalculadoraControllerTest.java
@@ -1,6 +1,5 @@
 package demoapp;
 
-
 import demoapp.service.CalculadoraService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -28,38 +25,28 @@ public class CalculadoraControllerTest {
     private CalculadoraService calculadoraService;
 
     @Test
-    public void shouldReturnCorrectResult() throws Exception {
-        when(calculadoraService.operar(3, 4, '+')).thenReturn(7.0);
+    public void testValidOperandsAndOperator() throws Exception {
+        // Mock the CalculadoraService
+        when(calculadoraService.operar(1.0, 2.0, '+')).thenReturn(3.0);
 
         this.mockMvc.perform(post("/calculadora/calcular")
-                        .param("operando1", "3")
-                        .param("operando2", "4")
+                        .param("operando1", "1.0")
+                        .param("operando2", "2.0")
                         .param("operador", "+"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Resultado: <span>7.0</span>")));
+                .andExpect(content().string(containsString("Resultado: <span>3.0</span>")));
     }
 
     @Test
-    public void shouldReturnDivideByZeroError() throws Exception {
-        when(calculadoraService.operar(anyDouble(), eq(0.0), eq('/'))).thenThrow(new ArithmeticException("No se puede dividir por cero"));
+    public void testDivisionByZero() throws Exception {
+        // Mock the CalculadoraService to throw ArithmeticException for division by zero
+        when(calculadoraService.operar(1.0, 0.0, '/')).thenThrow(new ArithmeticException());
 
         this.mockMvc.perform(post("/calculadora/calcular")
-                        .param("operando1", "3")
-                        .param("operando2", "0")
+                        .param("operando1", "1.0")
+                        .param("operando2", "0.0")
                         .param("operador", "/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("No se puede dividir por cero")));
-    }
-
-    @Test
-    public void shouldReturnInvalidOperationError() throws Exception {
-        when(calculadoraService.operar(anyDouble(), anyDouble(), eq('x'))).thenThrow(new IllegalArgumentException("Operación no válida"));
-
-        this.mockMvc.perform(post("/calculadora/calcular")
-                        .param("operando1", "3")
-                        .param("operando2", "4")
-                        .param("operador", "x"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Operador no válido")));
     }
 }
